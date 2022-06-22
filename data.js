06e7f016fc6b18f147b810a8a8a006c8270cc0d5
Danh Tran <dinhdanh183@gmail.com>
2022-06-22 17:38:44 +0700

    creat music

---
+++ data.js
@@ -0,0 +1,26 @@
+let songs = [
+    {
+        name: '1 Vòng',
+        path: './music-item/music1.mp3',
+        artist: 'Tùng Tea',
+        cover: 'img/1vong.jpg'
+    },
+    {
+        name: 'His Story',
+        path: './music-item/music2.mp3',
+        artist: 'Robe',
+        cover: 'img/his story.jpg'
+    },
+    {
+        name: 'Phiêu Bồng',
+        path: './music-item/music3.mp3',
+        artist: 'Tofu',
+        cover: 'img/phieubong.jpg'
+    },
+    {
+        name: 'Query',
+        path: './music-item/music4.mp3',
+        artist: 'QNT - MCK - Trung Trần',
+        cover: 'img/query.jpg'
+    }
+]
